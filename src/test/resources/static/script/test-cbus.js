8ec85f8e9ea98e31ad7417ec5c363697f9f035b9
david elllis <david@ellis128.co.uk>
2020-02-14 10:28:45 +0000

    refined first two test cases

--- src/test/resources/static/script/test-cbus.js
+++ src/test/resources/static/script/test-cbus.js
@@ -3,16 +3,16 @@ describe("test cbus", function() {
 	describe("test cbusProtocol", function() {
 		
 		const test_cases = [
-			{ description: "MIN_VALUE should return unknown", input: Number.MIN_VALUE, result: "unknown"},
-			{ description: "0 should return unknown", input: 0, result: "unknown"},
-			{ description: "1 should return CAN", input: 1, result: "CAN"},
-			{ description: "2 should return ETH", input: 2, result: "ETH"},
-			{ description: "3 should return unknown", input: 3, result: "unknown"},
-			{ description: "MAX_VALUE should return unknown", input: Number.MAX_VALUE, result: "unknown"},
+			{ input: Number.MIN_VALUE, result: "unknown"},
+			{ input: 0, result: "unknown"},
+			{ input: 1, result: "CAN"},
+			{ input: 2, result: "ETH"},
+			{ input: 3, result: "unknown"},
+			{ input: Number.MAX_VALUE, result: "unknown"},
 		];
 		
 		test_cases.forEach((parameter) => {
-		  it(parameter.description, () => {
+		  it(parameter.input + " should return " + parameter.result, () => {
 		    expect(cbusProtocol(parameter.input)).toBe(parameter.result);
 		  });
 		});
@@ -21,31 +21,24 @@ describe("test cbus", function() {
 	describe("test cbusManufacturer", function() {
 		
 		const test_cases = [
-			{ description: "MIN_VALUE should return unknown", input: Number.MIN_VALUE, result: "unknown"},
-			{ description: "69 should return unknown", input: 69, result: "unknown"},
-			{ description: "70 should return ROCRAIL", input: 70, result: "ROCRAIL"},
-			{ description: "71 should return unknown", input: 71, result: "unknown"},
-			{ description: "79 should return unknown", input: 79, result: "unknown"},
-			{ description: "80 should return SPECTRUM", input: 80, result: "SPECTRUM"},
-			{ description: "81 should return unknown", input: 81, result: "unknown"},
-			{ description: "164 should return unknown", input: 164, result: "unknown"},
-			{ description: "165 should return MERG", input: 165, result: "MERG"},
-			{ description: "166 should return unknown", input: 166, result: "unknown"},
-			{ description: "MAX_VALUE should return unknown", input: Number.MAX_VALUE, result: "unknown"},
+			{ input: Number.MIN_VALUE, result: "unknown"},
+			{ input: 69, result: "unknown"},
+			{ input: 70, result: "ROCRAIL"},
+			{ input: 71, result: "unknown"},
+			{ input: 79, result: "unknown"},
+			{ input: 80, result: "SPECTRUM"},
+			{ input: 81, result: "unknown"},
+			{ input: 164, result: "unknown"},
+			{ input: 165, result: "MERG"},
+			{ input: 166, result: "unknown"},
+			{ input: Number.MAX_VALUE, result: "unknown"},
 		];
 		
 		test_cases.forEach((parameter) => {
-		  it(parameter.description, () => {
+		  it(parameter.input + " should return " + parameter.result, () => {
 		    expect(cbusManufacturer(parameter.input)).toBe(parameter.result);
 		  });
 		});
 	});
-
-
-	// single test case, using arrow function
-	  it("Single test example", () => {
-	    expect(1).toBe(1);
-	  });
-
 
 });
